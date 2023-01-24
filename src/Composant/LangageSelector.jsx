af9642edb9b4554a6993c8a240e2bcd8c599eb9c
Nefferou <fertijulien@gmail.com>
2023-01-24 17:23:45 +0100

    LangSelector

--- src/Composant/LangageSelector.jsx
+++ src/Composant/LangageSelector.jsx
@@ -1,22 +1,21 @@
 import React from "react";
+import "../Style/style.css"
 
 function LangageSelector({changeLang}) {
     return (
         <>
-            <tr>
-                <td><button onClick={() => changeLang("fr")}>fr</button></td>
-                <td><button onClick={() => changeLang("en")}>en</button></td>
-                <td><button onClick={() => changeLang("it")}>it</button></td>
-            </tr>
-            <tr>
-                <td><button onClick={() => changeLang("es")}>es</button></td>
-                <td><button onClick={() => changeLang("de")}>de</button></td>
-                <td><button onClick={() => changeLang("ja")}>ja</button></td>
-            </tr>
-            <tr>
-                <td><button onClick={() => changeLang("zh-hans")}>zh-hans</button></td>
-                <td><button onClick={() => changeLang("roomaji")}>roomaji</button></td>
-                <td><button onClick={() => changeLang("ko")}>ko</button></td>
+            <tr className="langSelector">
+                <button onClick={() => changeLang("fr")}>fr</button>
+                <button onClick={() => changeLang("en")}>en</button>
+                <button onClick={() => changeLang("it")}>it</button>
+
+                <button onClick={() => changeLang("es")}>es</button>
+                <button onClick={() => changeLang("de")}>de</button>
+                <button onClick={() => changeLang("ja")}>ja</button>
+
+                <button onClick={() => changeLang("zh-hans")}>zh-hans</button>
+                <button onClick={() => changeLang("roomaji")}>roomaji</button>
+                <button onClick={() => changeLang("ko")}>ko</button>
             </tr>
         </>
     );
