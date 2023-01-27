5e1c9e6afd7305bb5a317b29b77b876f832059e2
Nefferou <61784669+Nefferou@users.noreply.github.com>
2023-01-27 09:07:58 +0100

    Update Header.jsx

--- src/Composant/Header.jsx
+++ src/Composant/Header.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import LangageSelector from "./LangageSelector";
+import pokedex from "../Ressources/pokedex.png"
 import "../Style/style.css"
 
 function Header({lang, changeLang}) {
     return (
         <div id="header">
-            <img />
+            <img src={pokedex}/>
             <div></div>
             <LangageSelector changeLang={changeLang} />
         </div>
